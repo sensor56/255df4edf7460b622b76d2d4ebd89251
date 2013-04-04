ba90bd30d5d5410a73f5bd2e1da9031e27e07ce1
monclubelec <support@mon-club-elec.fr>
2013-04-04 16:50:49 +0300

    Create prog3.ino

---
+++ prog3.ino
@@ -0,0 +1,5 @@
+// Supports PDF d'Ateliers Arduino par www.mon-club-elec.fr
+// http://www.mon-club-elec.fr/pmwiki_mon_club_elec/pmwiki.php?n=MAIN.ATELIERS
+// par X. HINAULT - tous droits réservés - 2012 - GPLv3
+
+// code : 17. Technique : Remarque sur les capteurs ON/OFF « mécaniques » et les interruptions externes
